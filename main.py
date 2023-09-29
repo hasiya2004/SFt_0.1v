2d4e20d07e779fcbd74757c5d13f6535cde69c0a
Hasindu Senarathna <85918275+hasiya2004@users.noreply.github.com>
2023-09-29 15:30:35 -0400

    Update main.py
    add moon data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+'''import pandas as pd
 import numpy as np
 
 # Seed for reproducibility
@@ -21,4 +21,37 @@ df = pd.DataFrame(data)
 # Calculate Total_Sale column
 df['Total_Sale'] = df['Quantity_Sold'] * df['Price_Per_Unit']
 
-print(df)
+print(df) '''
+
+import pandas as pd
+import numpy as np
+
+# Seed for reproducibility
+np.random.seed(42)
+
+# Sample sales data
+data = {
+    'Date': pd.date_range(start='2022-01-01', periods=10, freq='D'),
+    'Product_ID': np.random.randint(100, 110, 10),
+    'Quantity_Sold': np.random.randint(1, 10, 10),
+    'Price_Per_Unit': np.random.uniform(5.0, 20.0, 10),
+    'pH_Value': np.random.uniform(5.5, 8.5, 10)
+}
+
+# Creating the sales DataFrame
+df = pd.DataFrame(data)
+df['Total_Sale'] = df['Quantity_Sold'] * df['Price_Per_Unit']
+
+# NASA moon landing data
+moon_landings = {
+    'Mission': ['Apollo 11', 'Apollo 12', 'Apollo 14', 'Apollo 15', 'Apollo 16', 'Apollo 17'],
+    'Date': ['1969-07-20', '1969-11-19', '1971-02-05', '1971-07-30', '1972-04-21', '1972-12-11'],
+    'Astronauts': [['Neil Armstrong', 'Buzz Aldrin'], ['Pete Conrad', 'Alan Bean'], ['Alan Shepard', 'Edgar Mitchell'], ['David Scott', 'James Irwin'], ['John Young', 'Charles Duke'], ['Gene Cernan', 'Harrison Schmitt']]
+}
+
+moon_df = pd.DataFrame(moon_landings)
+
+# Merging the two datasets on the 'Date' column
+merged_df = pd.merge(df, moon_df, on='Date', how='left')
+
+print(merged_df)
