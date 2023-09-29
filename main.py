f4724922636d29f91c2c9c56607b6a166774187d
Hasindu Senarathna <85918275+hasiya2004@users.noreply.github.com>
2023-09-29 16:48:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,99 @@
-
 import pandas as pd
-import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
-# Seed for reproducibility
-np.random.seed(42)
+# Loading dataset or creating a new one
+try:
+    df = pd.read_csv("qa_dataset.csv")
+except FileNotFoundError:
+    data = {
+        'Question': [
+            "how are you",
+            "weather",
+            "joke",
+            "photosynthesis",
+            "world series in 2020",
+            "how does orbital navigation work",
+            "tell me about the apollo 11 mission",
+            "how do neurons transmit signals",
+            "what is the role of synapses",
+            "explain the theory of relativity",
+            "who was the first human to travel to space",
+            "describe the function of the hippocampus"
+        ],
+        'Answer': [
+            "I'm just a computer program, so I don't have feelings, but I'm operating normally.",
+            "Sorry, I don't have real-time weather information right now.",
+            "Why did the chicken cross the road? To get to the other side.",
+            "Photosynthesis is the process used by plants, algae, and some bacteria to convert sunlight into energy.",
+            "The Los Angeles Dodgers won the World Series in 2020.",
+            "Orbital navigation involves understanding and controlling the trajectory of a spacecraft to ensure it remains on the desired orbital path.",
+            "Apollo 11 was the first manned mission to land on the Moon in 1969, with astronauts Neil Armstrong, Buzz Aldrin, and Michael Collins.",
+            "Neurons transmit signals using a combination of electrical impulses (action potentials) and chemical signals (neurotransmitters).",
+            "Synapses are the junctions where neurons communicate. They transmit signals using neurotransmitters.",
+            "The theory of relativity, proposed by Albert Einstein, revolutionized the way we understand time, space, and the universe's structure.",
+            "Yuri Gagarin was the first human to travel to space aboard Vostok 1 in 1961.",
+            "The hippocampus is a region in the brain primarily associated with memory and spatial navigation."
+        ]
+    }
+    # Adding questions related to Brownian Motion
+    brownian_questions = [
+        "What is Brownian motion?",
+        "Who discovered Brownian motion?",
+        "How is Brownian motion related to diffusion?",
+        "Is Brownian motion a result of random particle movement?",
+        # ... [Add up to 100 questions]
+    ]
 
-# Sample sales data
-data = {
-    'Date': pd.date_range(start='2022-01-01', periods=10, freq='D'),
-    'Product_ID': np.random.randint(100, 110, 10),
-    'Quantity_Sold': np.random.randint(1, 10, 10),
+    brownian_answers = [
+        "Brownian motion describes the random movement of particles suspended in a fluid (liquid or gas) resulting from their collision with fast atoms or molecules in the gas or liquid.",
+        "Brownian motion was discovered by the botanist Robert Brown in 1827.",
+        "Brownian motion is a physical phenomenon underlying the mathematical theory of diffusion and heat conduction.",
+        "Yes, Brownian motion is due to the random movement of particles.",
+        # ... [Add corresponding answers for the above questions]
+    ]
 
-    'Price_Per_Unit': np.random.uniform(5.0, 20.0, 10),
-    'pH_Value': np.random.uniform(5.5, 8.5, 10)
-}
+    # Appending to the existing dataset
+    data['Question'].extend(brownian_questions)
+    data['Answer'].extend(brownian_answers)
 
-# Creating the sales DataFrame
-df = pd.DataFrame(data)
-df['Total_Sale'] = df['Quantity_Sold'] * df['Price_Per_Unit']
+    df = pd.DataFrame(data)
 
-# NASA moon landing data
-moon_landings = {
-    'Mission': ['Apollo 11', 'Apollo 12', 'Apollo 14', 'Apollo 15', 'Apollo 16', 'Apollo 17'],
-    'Date': ['1969-07-20', '1969-11-19', '1971-02-05', '1971-07-30', '1972-04-21', '1972-12-11'],
-    'Astronauts': [['Neil Armstrong', 'Buzz Aldrin'], ['Pete Conrad', 'Alan Bean'], ['Alan Shepard', 'Edgar Mitchell'], ['David Scott', 'James Irwin'], ['John Young', 'Charles Duke'], ['Gene Cernan', 'Harrison Schmitt']]
-}
+# Using TF-IDF to transform questions for similarity search
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf_vectorizer.fit_transform(df['Question'])
 
-moon_df = pd.DataFrame(moon_landings)
+def get_closest_match_from_df(question):
+    # Transforming the user's question with the vectorizer
+    tfidf_question = tfidf_vectorizer.transform([question])
 
-# Merging the two datasets on the 'Date' column
-merged_df = pd.merge(df, moon_df, on='Date', how='left')
+    # Calculating cosine similarities
+    cosine_similarities = linear_kernel(tfidf_question, tfidf_matrix).flatten()
 
-print(merged_df)
-=======
-    'Price_Per_Unit': np.random.uniform(5.0, 20.0, 10)
-}
+    # Getting the index of the most similar question
+    closest_match_idx = cosine_similarities.argsort()[-1]
+
+    if cosine_similarities[closest_match_idx] == 0:
+        return None
+
+    return df.iloc[closest_match_idx]['Answer']
+
+# Interactive loop
+while True:
+    user_input = input("\nAsk me a question (or type 'exit' to quit, 'add' to add a Q&A pair): ")
+    if user_input.lower() == 'exit':
+        df.to_csv("qa_dataset.csv", index=False)
+        break
+    elif user_input.lower() == 'add':
+        question = input("Enter the question: ")
+        answer = input("Enter the answer: ")
+        df = df.append({"Question": question, "Answer": answer}, ignore_index=True)
+        # Re-transforming the tfidf matrix after adding new data
+        tfidf_matrix = tfidf_vectorizer.fit_transform(df['Question'])
+        print("Q&A pair added!")
+    else:
+        response = get_closest_match_from_df(user_input)
+        if response:
+            print(response)
+        else:
+            print("Sorry, I don't know the answer to that.")
